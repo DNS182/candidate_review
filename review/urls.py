d9d2370918038f373ef5786eb3ae358d7e6ef341
Dinesh <182dns@gmail.com>
2022-05-19 11:02:41 +0545

    collectstatic

--- review/urls.py
+++ review/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 admin.site.site_header = "ONLY 182 ENTRY"  
 admin.site.index_title = 'VIP ONLY'              
@@ -25,4 +29,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , include('candidate.urls')) ,
     path('api/' , include('api.urls'))
-]
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
