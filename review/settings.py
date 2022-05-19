05a8e7c59aca5e09034310186e1f8e2aba05ca99
Dinesh <182dns@gmail.com>
2022-05-19 18:11:02 +0545

    added some css

--- review/settings.py
+++ review/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'dsjkjdsghfkjdhfkjshdjkfhsjkdfhsajdf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -50,8 +50,8 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',   
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
